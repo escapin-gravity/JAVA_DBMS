cf555fa0cde23b0580aa04ae4445ac3eb1262335
Yu seung min <seowol55@naver.com>
2024-03-03 06:31:43 +0900

    Add enhanced GUI and functionality

--- src/UI/DBMSApp.java
+++ src/UI/DBMSApp.java
@@ -3,85 +3,97 @@ package UI;
 import logic.SimpleDBMS;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
 
 public class DBMSApp extends JFrame {
-    private SimpleDBMS db;
+    private SimpleDBMS database;
     private JTextField keyField;
     private JTextField valueField;
-    private JTextArea outputArea;
+    private JTextArea logArea;
+    private JTable dataTable;
 
     public DBMSApp() {
-        db = new SimpleDBMS();
+        database = new SimpleDBMS();
 
-        setTitle("Simple DBMS");
-        setSize(400, 300);
+        setTitle("Relational Database");
+        setSize(1000, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
 
         JPanel inputPanel = createInputPanel();
-        add(inputPanel, BorderLayout.NORTH);
+        add(inputPanel, BorderLayout.WEST);
 
-        outputArea = new JTextArea();
-        add(new JScrollPane(outputArea), BorderLayout.CENTER);
+        JPanel logPanel = createLogPanel();
+        add(logPanel, BorderLayout.CENTER);
+
+        JPanel dataPanel = createDataPanel();
+        add(dataPanel, BorderLayout.EAST);
     }
 
     private JPanel createInputPanel() {
-        JPanel inputPanel = new JPanel(new GridLayout(4, 2));
+        JPanel inputPanel = new JPanel(new GridLayout(0, 2, 5, 5));
+        inputPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
         inputPanel.add(new JLabel("Key:"));
         keyField = new JTextField();
         inputPanel.add(keyField);
+
         inputPanel.add(new JLabel("Value:"));
         valueField = new JTextField();
         inputPanel.add(valueField);
 
-        JButton addButton = new JButton("Add");
-        addButton.addActionListener(new ActionListener() {
+        JButton insertButton = new JButton("Insert");
+        insertButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String key = keyField.getText();
                 String value = valueField.getText();
                 if (!key.isEmpty() && !value.isEmpty()) {
-                    db.put(key, value);
-                    outputArea.append("Added: " + key + " - " + value + "\n");
+                    database.insertData(key, value);
+                    logArea.append("Inserted: " + key + " - " + value + "\n");
+                    clearFields();
+                    updateData();
                 } else {
                     JOptionPane.showMessageDialog(DBMSApp.this, "Key and value cannot be empty");
                 }
             }
         });
-        inputPanel.add(addButton);
+        inputPanel.add(insertButton);
 
-        JButton getButton = new JButton("Get");
-        getButton.addActionListener(new ActionListener() {
+        JButton retrieveButton = new JButton("Retrieve");
+        retrieveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String key = keyField.getText();
-                String value = db.get(key);
-                if (value != null) {
-                    outputArea.append("Value for key " + key + ": " + value + "\n");
+                if (!key.isEmpty()) {
+                    String value = database.getData(key);
+                    if (value != null) {
+                        logArea.append("Retrieved value for key " + key + ": " + value + "\n");
+                    } else {
+                        logArea.append("No value found for key " + key + "\n");
+                    }
                 } else {
-                    outputArea.append("No value found for key " + key + "\n");
+                    JOptionPane.showMessageDialog(DBMSApp.this, "Key cannot be empty");
                 }
             }
         });
-        inputPanel.add(getButton);
+        inputPanel.add(retrieveButton);
 
         JButton updateButton = new JButton("Update");
         updateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String key = keyField.getText();
-                String newValue = valueField.getText();
-                if (!key.isEmpty() && !newValue.isEmpty()) {
-                    String oldValue = db.get(key);
-                    if (oldValue != null) {
-                        db.put(key, newValue);
-                        outputArea.append("Updated: " + key + " - " + oldValue + " to " + newValue + "\n");
-                    } else {
-                        outputArea.append("No value found for key " + key + "\n");
-                    }
+                String value = valueField.getText();
+                if (!key.isEmpty() && !value.isEmpty()) {
+                    database.updateData(key, value);
+                    logArea.append("Updated value for key " + key + ": " + value + "\n");
+                    clearFields();
+                    updateData();
                 } else {
                     JOptionPane.showMessageDialog(DBMSApp.this, "Key and value cannot be empty");
                 }
@@ -89,15 +101,66 @@ public class DBMSApp extends JFrame {
         });
         inputPanel.add(updateButton);
 
+        JButton deleteButton = new JButton("Delete");
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String key = keyField.getText();
+                if (!key.isEmpty()) {
+                    database.deleteData(key);
+                    logArea.append("Deleted key: " + key + "\n");
+                    clearFields();
+                    updateData();
+                } else {
+                    JOptionPane.showMessageDialog(DBMSApp.this, "Key cannot be empty");
+                }
+            }
+        });
+        inputPanel.add(deleteButton);
+
         return inputPanel;
     }
 
+    private JPanel createLogPanel() {
+        JPanel logPanel = new JPanel(new BorderLayout());
+        logPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        logArea = new JTextArea();
+        logArea.setEditable(false);
+        logPanel.add(new JScrollPane(logArea), BorderLayout.CENTER);
+        return logPanel;
+    }
+
+    private JPanel createDataPanel() {
+        JPanel dataPanel = new JPanel(new BorderLayout());
+        dataPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        DefaultTableModel model = new DefaultTableModel(new String[]{"Key", "Value"}, 0);
+        dataTable = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(dataTable);
+        dataPanel.add(scrollPane, BorderLayout.CENTER);
+
+        return dataPanel;
+    }
+
+    private void updateData() {
+        DefaultTableModel model = (DefaultTableModel) dataTable.getModel();
+        model.setRowCount(0);
+        Map<String, String> data = database.getAllData();
+        for (Map.Entry<String, String> entry : data.entrySet()) {
+            model.addRow(new String[]{entry.getKey(), entry.getValue()});
+        }
+    }
+
+    private void clearFields() {
+        keyField.setText("");
+        valueField.setText("");
+    }
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new DBMSApp().setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            DBMSApp gui = new DBMSApp();
+            gui.setVisible(true);
         });
     }
 }
+
--- src/logic/SimpleDBMS.java
+++ src/logic/SimpleDBMS.java
@@ -1,33 +1,42 @@
-package logic; // 이 구문이 있어야 컴파일러가 인식
+package logic;
 
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SimpleDBMS {
-    private Hashtable<String, String> dataStore;
+    private Map<String, String> dataMap;
 
     public SimpleDBMS() {
-        dataStore = new Hashtable<>();
+        dataMap = new HashMap<>();
     }
 
-    public void put(String key, String value) {
-        dataStore.put(key, value);
+    public void insertData(String key, String value) {
+        dataMap.put(key, value);
     }
 
-    public String get(String key) {
-        return dataStore.get(key);
+    public String getData(String key) {
+        return dataMap.get(key);
     }
 
-    public boolean containsKey(String key) {
-        return dataStore.containsKey(key);
+    public Map<String, String> getAllData() {
+        return new HashMap<>(dataMap);
     }
 
-    public String remove(String key) {
-        return dataStore.remove(key);
+    public void updateData(String key, String value) {
+        if (dataMap.containsKey(key)) {
+            dataMap.put(key, value);
+        } else {
+            System.out.println("Key not found");
+        }
     }
-    
-    public void update(String key, String newValue) {
-        if (dataStore.containsKey(key)) {
-            dataStore.put(key, newValue);
+
+    public void deleteData(String key) {
+        if (dataMap.containsKey(key)) {
+            dataMap.remove(key);
+        } else {
+            System.out.println("Key not found");
         }
     }
 }
+
+
